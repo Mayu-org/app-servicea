e0ef088361423cf9e9d2f289e19420134db96429
Mayu <Mayu@DESKTOP-GRFN1CA>
2024-07-22 22:38:49 +0100

    added opentel support

--- servicea/src/main/java/com/example/servicea/ServiceAApplication.java
+++ servicea/src/main/java/com/example/servicea/ServiceAApplication.java
@@ -5,9 +5,19 @@ import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ServiceAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAApplication.class, args);
+    }
+}
+
 
 @RestController
 class ServiceAController {
