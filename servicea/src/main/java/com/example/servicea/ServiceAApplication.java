eb13ba80833c8d9127248299bb426a85a3006a78
Mayu <Mayu@DESKTOP-GRFN1CA>
2024-07-22 23:17:29 +0100

    added opentel support

--- servicea/src/main/java/com/example/servicea/ServiceAApplication.java
+++ servicea/src/main/java/com/example/servicea/ServiceAApplication.java
@@ -1,10 +1,8 @@
 package com.example.servicea;
 
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Context;
-import org.springframework.beans.factory.annotation.Value;
+
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,33 +11,26 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ServiceAApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ServiceAApplication.class, args);
     }
 }
 
-
 @RestController
 class ServiceAController {
-    private final RestTemplate restTemplate;
-    private final Tracer tracer;
-    
-    @Value("${service-b.url}")
-    private String serviceBUrl;
 
-    public ServiceAController(RestTemplate restTemplate, OpenTelemetry openTelemetry) {
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public ServiceAController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.tracer = openTelemetry.getTracer(ServiceAController.class.getName());
     }
 
     @GetMapping("/data")
+    @WithSpan
     public String getData() {
-        Span span = tracer.spanBuilder("getData").startSpan();
-        try (var scope = span.makeCurrent()) {
-            String serviceBResponse = restTemplate.getForObject(serviceBUrl + "/info", String.class);
-            return "Data from Service B: " + serviceBResponse;
-        } finally {
-            span.end();
-        }
+        String serviceBResponse = restTemplate.getForObject("http://service-b.myapp1.svc.cluster.local:8081/info", String.class);
+        return "Data from Service B: " + serviceBResponse;
     }
 }
